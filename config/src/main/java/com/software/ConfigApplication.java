fc0d5aaffe2f1ce1e541d14e420f5efaaed837e0
高虎 <gaohu9712@163.com>
2020-11-10 10:53:14 +0800

    init commit

---
+++ config/src/main/java/com/software/ConfigApplication.java
@@ -0,0 +1,21 @@
+package com.software;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author gaohu9712@163.com
+ * @date 2020/11/10
+ * @description
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigApplication.class, args);
+    }
+}
---
+++ model/info/src/main/java/com/software/controller/InfoController.java
@@ -0,0 +1,29 @@
+package com.software.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @author gaohu9712@163.com
+ * @date 2020/11/10
+ * @description
+ */
+@RestController
+@RequestMapping("/info")
+public class InfoController {
+    @Value("${sql.name}")
+    private String name;
+
+    @Value("${sql.version}")
+    private String version;
+
+    @GetMapping("/get")
+    public Object get() {
+
+        return "名称：" + name + ", 版本：" + version;
+    }
+}
