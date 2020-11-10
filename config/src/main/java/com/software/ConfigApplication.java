1b8a070a926ec24095e974abd854b37b8eea8f08
高虎 <gaohu9712@163.com>
2020-11-10 15:52:04 +0800

    配置中心

--- config/src/main/java/com/software/ConfigApplication.java
+++ config/src/main/java/com/software/ConfigApplication.java
@@ -14,6 +14,7 @@ import org.springframework.cloud.config.server.EnableConfigServer;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class ConfigApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigApplication.class, args);
